c1f6dafd30ba507ece782484dd0556fa2d1c21b2
grisha <norsulfasol@gmail.com>
2022-09-30 00:41:06 +0300

    Первая часть второго ДЗ

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание1"); // первое задание
+    var dog = 8.0;
+    System.out.println(dog);
+    var cat = 3.6;
+    System.out.println(cat);
+    var paper = 763789;
+    System.out.println(paper);
+
+    System.out.println("Задание2"); // второе задание
+     dog+=4;
+     System.out.println(dog);
+     cat+=4;
+     System.out.println(cat);
+     paper+=4;
+     System.out.println(paper);
+
+     System.out.println("Задание3");
+     dog-=3.5;
+     System.out.println(dog);
+     cat-=1.6;
+     System.out.println(cat);
+     paper-=7639;
+     System.out.println(paper);
+    }
+}
